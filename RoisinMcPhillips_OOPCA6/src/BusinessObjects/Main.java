7081326985f31c4ca5e27ace5e936c6839646e90
D00219161 <57397919+D00219161@users.noreply.github.com>
2020-05-06 23:58:56 +0100

    CA6 Files - Got GetRegisteredVehicles loaded in from file on server side and sent over to the client. Added to a set in server side, and json to string working. Got the RegisterVaildTollEvent started and added to client side. Got the DTO TollEvent file linked in.

--- RoisinMcPhillips_OOPCA6/src/BusinessObjects/Main.java
+++ RoisinMcPhillips_OOPCA6/src/BusinessObjects/Main.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Main
 {
-
     public static void main(String[] args)
     {
         TollEventDaoInterface IEventDao = new MySqlTollEventDao(); //I for interface
